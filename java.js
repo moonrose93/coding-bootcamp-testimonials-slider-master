efabf4f6ba0046837abdab4f33ba4f42469875e0
Isidora Stanisavljevska <137770804+moonrose93@users.noreply.github.com>
2023-08-07 16:19:08 +0200

    Update java.js

--- java.js
+++ java.js
@@ -11,7 +11,7 @@ btnElNext.addEventListener("click", nextPerson);
 btnElPrev.addEventListener("click", prevPerson);
 
 function nextPerson() {
-    personImage.src = "images/image-john.jpg";
+    personImage.src = "/image-john.jpg";
     quoteElDesktop.innerHTML = `“ If you want to lay the best foundation possible I’d recommend taking this course. 
     The depth the instructors go into is incredible. I now feel so confident about 
     starting up as a professional developer. ”`;
@@ -23,7 +23,7 @@ function nextPerson() {
 }
 
 function prevPerson() {
-    personImage.src = "images/image-tanya.jpg";
+    personImage.src = "/image-tanya.jpg";
     quoteElDesktop.innerHTML = `“ I’ve been interested in coding for a while but never taken the jump, until now. 
     I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
     excited about the future. ”`;
